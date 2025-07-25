46ac43a00dc19bdc4b330e9686644497fd75f066
broisnischal <info@nischal-dahal.com.np>
2025-07-25 16:23:44 +0545

    asdf

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,7 +3,7 @@ import { Elysia } from "elysia";
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
   .get("/health", () => "OK")
-  .listen(process.env.API_PORT || 8000);
+  .listen(process.env.API_PORT || 4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
